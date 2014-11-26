a7c33e0045cda8b745c14858774210d8a0be1779
Raúl <rsaaperez@danielcastelao.org>
2014-11-26 10:54:05 +0100

    Actualizado con ventana de visualizacion empleando ListActivity

--- app/src/main/java/com/rsaaperez/ral/intercambiodatos/ListaActivity.java
+++ app/src/main/java/com/rsaaperez/ral/intercambiodatos/ListaActivity.java
@@ -1,6 +1,7 @@
 package com.rsaaperez.ral.intercambiodatos;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,7 +17,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 
-public class ListaActivity extends Activity {
+public class ListaActivity extends ListActivity {
 
     ArrayList<Agenda> contactos;
 
@@ -24,44 +25,30 @@ public class ListaActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
-
-        final Intent intento = new Intent(ListaActivity.this, MyActivity2.class);
         contactos = (ArrayList<Agenda>) getIntent().getSerializableExtra("id1");
-
+        /*
         ListView lista = (ListView) findViewById(R.id.lista);
         lista.setAdapter(new ArrayAdapter<Agenda>(this, android.R.layout.simple_list_item_1, contactos));
         lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent intento = new Intent(ListaActivity.this, MyActivity2.class);
                 intento.putExtra("id2", new Agenda (contactos.get(i).getNombre(),contactos.get(i).getTelefono()));
                 intento.putExtra("id3", contactos);
                 startActivityForResult(intento, 1);
             }
         });
+        */
+        setListAdapter(new ArrayAdapter<Agenda>(this,android.R.layout.simple_list_item_1, contactos));
+    }
+    public void onListItemClick(ListView parent,View v,int posicion,long id){
+        Intent intento = new Intent(ListaActivity.this, MyActivity2.class);
+        intento.putExtra("id2", new Agenda (contactos.get(posicion).getNombre(),contactos.get(posicion).getTelefono()));
+        intento.putExtra("id3", contactos);
+        startActivityForResult(intento, 1);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lista, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
