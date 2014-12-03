2e8ab7e7154db25d2b4793e9fd746c87b1c82b40
Raúl <rsaaperez@danielcastelao.org>
2014-12-03 11:17:07 +0100

    Commit por que si

--- app/src/main/java/com/rsaaperez/ral/intercambiodatos/MyActivity.java
+++ app/src/main/java/com/rsaaperez/ral/intercambiodatos/MyActivity.java
@@ -65,6 +65,7 @@ public class MyActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        //
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
